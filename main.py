a3a219ee6544b47018dda3b245fe85c4f1924721
futecom <alexandredario@hotmail.com>
2023-12-03 20:02:05 +0000

    small fixes and adding exceptions

--- main.py
+++ main.py
@@ -41,9 +41,11 @@ def get_data_selenium(selenium_url, headless=False):
     try:
         browser.get(selenium_url)
     except TimeoutError as time_out_error:
-        print(f"{time_out_error}\nURL: {selenium_url}")
+        print(f"{time_out_error}\nURL: {selenium_url}\n")
     except selenium.common.exceptions.TimeoutException as time_out_exception:
-        print(f"{time_out_exception}\nURL: {selenium_url}")
+        print(f"{time_out_exception}\nURL: {selenium_url}\n")
+    except selenium.common.exceptions.NoSuchWindowException as window_exception:
+        print(f"{window_exception}URL: {selenium_url}\n")
     else:
         if "404" in browser.title:
             browser.close()
